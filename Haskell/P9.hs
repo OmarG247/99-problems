3d7f7d9a596b02fb7197248354bd718e2c7681ce
Omar Flores <omar.garciaf247@gmail.com>
2019-04-20 12:15:45 -0400

    problem 9 haskell

--- Haskell/P9.hs
+++ Haskell/P9.hs
@@ -1,1 +1,15 @@
--- Current
+-- Put together consecutive duplicates into lists
+pack :: (Eq a) => [a] -> [[a]]
+pack (a:b) 
+    = pack' (a:b) a []
+pack [] 
+    = []
+
+pack' :: (Eq a) => [a] -> a -> [a] -> [[a]]
+pack' (a:b) current temp
+    | a == current 
+        = pack' b current (a : temp)
+    | otherwise 
+        = temp : (pack' b a (a:[]))
+pack' [] _ temp
+    = [temp]
